8798fafb507d08454f5b5d00e7e272558374f797
Ryan Hernandez <ryan@rdhlab.com>
2022-06-27 19:39:09 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByuf_zBrkZ1EcYM97MKls-2bUGdzbddEE",
+  apiKey: "key",
   authDomain: "twighter-9fd35.firebaseapp.com",
   databaseURL: "https://twighter-9fd35-default-rtdb.firebaseio.com",
   projectId: "twighter-9fd35",
   storageBucket: "twighter-9fd35.appspot.com",
   messagingSenderId: "968841657949",
-  appId: "1:968841657949:web:ffc27624d92c65ecfd57b4",
+  appId: "API",
   measurementId: "G-GGEWK672DQ"
 };
 
